5b307994903927e72d528ed35aec76763b07d464
vagelis <rash>
2025-08-27 22:19:02 +0300

    some changes

--- install.sh
+++ install.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
+
 set -euo pipefail
+
+######################################       FUNCTIONS        ##########################################
+
+install_ghostty(){
+    echo "Installing Ghostty..."
+
+    curl -LO "https://download.opensuse.org/repositories/home:/clayrisser:/sid/Debian_Unstable/${ARCH}/ghostty_1.1.3-4_${ARCH}.deb"
+    sudo apt install -y "./ghostty_1.1.3-4_${ARCH}.deb"
+    rm "ghostty_1.1.3-4_${ARCH}.deb"
+}
+
+create_swaybg_service(){
+    echo "Installing swaybg"
+
+    sed -i "s|\$USER|$USER|g" swaybg.service
+    install -Dm644 swaybg.service $HOME/.config/systemd/user/swaybg.service
+    systemctl --user add-wants niri.service swaybg.service
+}
+
+create_swayidle_service(){
+    echo "Installing swayidle"
+
+    install -Dm644 swayidle.service $HOME/.config/systemd/user/swayidle.service
+    systemctl --user add-wants niri.service swayidle.service
+
+    systemctl --user daemon-reload
+}
+
+intall_niri(){
+
+    echo "Installing Niri..."
+    sudo install -Dm755 Niri/niri /usr/local/bin/niri
+    sudo install -Dm755 Niri/resources/niri-session /usr/local/bin/niri-session
+    sudo install -Dm644 Niri/resources/niri.desktop /usr/local/share/wayland-sessions/niri.desktop
+    sudo install -Dm644 Niri/resources/niri-portals.conf /usr/local/share/xdg-desktop-portal/niri-portals.conf
+    sudo install -Dm644 Niri/resources/niri.service /etc/systemd/user/niri.service
+    sudo install -Dm644 Niri/resources/niri-shutdown.target /etc/systemd/user/niri-shutdown.target
+
+}
+
+#####################################   START SCRIPT     #################################################
+
+
 
 echo "Starting installation..."
 
@@ -11,11 +55,7 @@ for cmd in curl dpkg sudo; do
     command -v $cmd >/dev/null 2>&1 || { echo "$cmd is required but not installed."; exit 1; }
 done
 
-# Install Ghostty terminal
-echo "Installing Ghostty..."
-curl -LO "https://download.opensuse.org/repositories/home:/clayrisser:/sid/Debian_Unstable/${ARCH}/ghostty_1.1.3-4_${ARCH}.deb"
-sudo apt install -y "./ghostty_1.1.3-4_${ARCH}.deb"
-rm "ghostty_1.1.3-4_${ARCH}.deb"
+install_ghostty
 
 # Install dependencies
 CORE_PACKAGES=(
@@ -36,8 +76,10 @@ CORE_PACKAGES=(
     wlogout
     swayidle
     greetd
+    # gtkgreet
     dunst
     mate-polkit-bin
+    #nwg-bar
 )
 
 OTHER_PACKAGES=(
@@ -56,44 +98,30 @@ OTHER_PACKAGES=(
 )
 
 echo "Installing core packages..."
-sudo apt update
+sudo apt update && sudo apt upgrade -y
 sudo apt install --no-install-recommends "${CORE_PACKAGES[@]}" -y
 
+mkdir -p $HOME/Pictures
+cp -rf ./Pictures/*.jpg $HOME/Pictures/
+mkdir -p $HOME/.config/systemd/user/
 
-# Install Niri
-echo "Installing Niri..."
-sudo install -Dm755 Niri/niri /usr/local/bin/niri
-sudo install -Dm755 Niri/resources/niri-session /usr/local/bin/niri-session
-sudo install -Dm644 Niri/resources/niri.desktop /usr/local/share/wayland-sessions/niri.desktop
-sudo install -Dm644 Niri/resources/niri-portals.conf /usr/local/share/xdg-desktop-portal/niri-portals.conf
-sudo install -Dm644 Niri/resources/niri.service /etc/systemd/user/niri.service
-sudo install -Dm644 Niri/resources/niri-shutdown.target /etc/systemd/user/niri-shutdown.target
+intall_niri
 
 echo "Installing waybar service"
 systemctl --user add-wants niri.service waybar.service
+cp -r waybar $HOME/.config/waybar
+
 
 # TODO: Install as a service
 #       dunst
 
-echo "Installing swaybg"
+#create_swaybg_service
 
-mkdir -p $HOME/Pictures
-cp -rf ./Pictures/*.jpg $HOME/Pictures/ ;
-mkdir -p $HOME/.config/systemd/user/
-sed -i "s|\$USER|$USER|g" swaybg.service
-install -Dm644 swaybg.service $HOME/.config/systemd/user/swaybg.service
-systemctl --user add-wants niri.service swaybg.service
+create_swayidle_service
 
-echo "Installing swayidle"
-
-install -Dm644 swayidle.service $HOME/.config/systemd/user/swayidle.service
-systemctl --user add-wants niri.service swayidle.service
-
-systemctl --user daemon-reload
-
-# Install Xwayland-satellite
 echo "Installing xwayland-satellite..."
 sudo install -Dm755 xwayland-satellite/xwayland-satellite /usr/local/bin/xwayland-satellite
+
 
 #echo "Installing polkit"
 
@@ -101,9 +129,8 @@ sudo install -Dm755 xwayland-satellite/xwayland-satellite /usr/local/bin/xwaylan
 echo "Setting up greetd, gtkgreet"
 
 # /etc/greetd/environments --- //niri \n bash
-sed -i "s|\$USER|$USER|g" greetd.config.toml
-sudo cp -rf greetd.config.toml /etc/greetd/config.toml
-
+# sed -i "s|\$USER|$USER|g" greetd.config.toml
+# sudo cp -rf greetd.config.toml /etc/greetd/config.toml
 
 
 read -p "Install Optional packages? (y/n) " -n 1 -r
@@ -145,5 +172,7 @@ fc-cache -fv
 
 echo "Fonts installed: ${FONTS[*]}"
 
+echo "Instllation completed"
 
 ###   base=$(basename $PWD) && cd .. && tar -czf $base.tar.gz $base && mv -f $base.tar.gz $base/$base.tar.gz && cd $base
+###   curl -LO http://192.168.122.1:3923/niri_setup.tar.gz && tar xf niri_setup.tar.gz && rm niri_setup.tar.gz && cd niri_setup && chmod +x install.sh
